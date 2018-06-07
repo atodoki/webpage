210bc9ea43e9b5a18222da4e8849c03ee67b1370
Matthew Yuen <yuenm18@up.edu>
2018-06-06 22:03:22 -0700

    Added preliminary github section

--- src/app/code/code.component.ts
+++ src/app/code/code.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { map } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-code',
@@ -6,10 +10,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./code.component.css']
 })
 export class CodeComponent implements OnInit {
+  githubUser: Observable<{avatar_url: string}>;
+  githubRepoList: Observable<{name: string, html_url: string}[]>;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.githubUser = this.http.get<{avatar_url: string}>('https://api.github.com/users/atodoki');
+    this.githubRepoList = this.http.get<
+      {
+        name: string,
+        html_url: string,
+        description: string,
+        language: string,
+        updated_at: string
+      }[]
+      >('https://api.github.com/users/atodoki/repos');
   }
 
 }
